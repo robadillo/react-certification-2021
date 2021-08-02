c38d001b8cb730c06deaf5cc1d3e08e82ced12e7
Rodrigo Hernández <onigirimex@gmail.com>
2021-08-01 22:23:39 -0500

    Cleaning the project

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>HOLA ENFERMERA</h1>
+    </div>
+  );
+}
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -5,33 +5,11 @@ import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
 
 function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
 
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
-    <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
-      {authenticated ? (
-        <>
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
-        </>
-      ) : (
-        <Link to="/login">let me in →</Link>
-      )}
+    <section className="homepage">
+      <h1>hola</h1>
     </section>
   );
 }
